40e929e12b61780c099a6b0f765a5c4d70c37c9f
Marcel Ngan <mpn10@vm-shell1.doc.ic.ac.uk>
2014-10-29 11:59:42 +0000

    coded first part of section 4

--- src/bookings/SeatReservationDemo.java
+++ src/bookings/SeatReservationDemo.java
@@ -3,6 +3,12 @@ package bookings;
 public class SeatReservationDemo {
 
     public static void main(String[] args) throws ReservationException {
+
+        SeatReservationManager manager = new SeatReservationManager()
+        for (int i = 0; i < 5; ++i){
+            Customer c = new Customer();
+            manager.reserveNextFree(c);
+        }
         
     }
     
